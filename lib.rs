a9fbb1a383b9c8ac721c45ada81fadf0d82b2b4f
Steven Chang <stevenchang5000@protonmail.com>
2023-10-10 18:27:00 +1000

    fn test_burn

--- lib.rs
+++ lib.rs
@@ -100,4 +100,63 @@ pub mod button {
             psp22::Internal::_burn_from(self, account, amount)
         }
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use ink::env::{
+            test::{default_accounts, set_caller, DefaultAccounts},
+            DefaultEnvironment,
+        };
+
+        // === HELPERS ===
+        fn init() -> (DefaultAccounts<DefaultEnvironment>, Button) {
+            let accounts = default_accounts();
+            set_caller::<DefaultEnvironment>(accounts.bob);
+            let az_button = Button::new(
+                28_000_000_000_000,
+                Some("Button".to_string()),
+                Some("BTN".to_string()),
+                6,
+            );
+            (accounts, az_button)
+        }
+
+        // === TEST HANDLES ===
+        #[ink::test]
+        fn test_burn() {
+            let (accounts, mut az_button) = init();
+            // when burning from own account
+            // = when balance is sufficient
+            // = * it burns the amount
+            az_button.burn(accounts.bob, 1_000_000_000_000).unwrap();
+            let mut balance: Balance = PSP22::balance_of(&az_button, accounts.bob);
+            assert_eq!(balance, 27_000_000_000_000);
+            // = when balance is insufficient
+            let mut result = az_button.burn(accounts.bob, 28_000_000_000_000);
+            // = * it raises an error
+            assert_eq!(result, Err(PSP22Error::InsufficientBalance));
+            // when burning from someone else's account
+            // = when balance is sufficient
+            // == when allowance is insufficient
+            set_caller::<DefaultEnvironment>(accounts.alice);
+            // == * it raises an error
+            result = az_button.burn(accounts.bob, 27_000_000_000_000);
+            assert_eq!(result, Err(PSP22Error::InsufficientAllowance));
+            // == when allowance is sufficient
+            set_caller::<DefaultEnvironment>(accounts.bob);
+            PSP22::increase_allowance(&mut az_button, accounts.alice, 28_000_000_000_000).unwrap();
+            set_caller::<DefaultEnvironment>(accounts.alice);
+            az_button.burn(accounts.bob, 1_000_000_000_000).unwrap();
+            // == * it burns the amount
+            balance = PSP22::balance_of(&az_button, accounts.bob);
+            assert_eq!(balance, 26_000_000_000_000);
+            // == * it decreases the allowance
+            let allowance: Balance = PSP22::allowance(&az_button, accounts.bob, accounts.alice);
+            assert_eq!(allowance, 27_000_000_000_000);
+            // === when balance is insufficient
+            result = az_button.burn(accounts.bob, 27_000_000_000_000);
+            assert_eq!(result, Err(PSP22Error::InsufficientBalance));
+        }
+    }
 }
