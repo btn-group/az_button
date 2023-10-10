f33afbfc33c31335acb5cafca01da05c74c06603
Steven Chang <stevenchang5000@gmail.com>
2023-10-10 17:54:04 +1000

    v3: Upgrade to openbrush v4 and set rules for burn (#6)

--- lib.rs
+++ lib.rs
@@ -1,33 +1,25 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
-#![feature(min_specialization)]
 
+#[openbrush::implementation(PSP22, PSP22Metadata, PSP22Capped)]
 #[openbrush::contract]
 pub mod button {
-    use ink::{
-        codegen::{EmitEvent, Env},
-        reflect::ContractEventBase,
-    };
-    use openbrush::{
-        contracts::psp22::{
-            extensions::{burnable::*, metadata::*},
-            Internal, PSP22Error, Transfer as PSP22Transfer,
-        },
-        traits::{Storage, String},
-    };
+    use ink::codegen::{EmitEvent, Env};
+    use openbrush::traits::Storage;
 
     // === EVENTS ===
+    /// Event emitted when a token transfer occurs.
     #[ink(event)]
-    #[derive(Debug)]
     pub struct Transfer {
         #[ink(topic)]
-        pub from: Option<AccountId>,
+        from: Option<AccountId>,
         #[ink(topic)]
-        pub to: Option<AccountId>,
-        pub value: Balance,
+        to: Option<AccountId>,
+        value: Balance,
     }
 
+    /// Event emitted when an approval occurs that `spender` is allowed to withdraw
+    /// up to the amount of `value` tokens from `owner`.
     #[ink(event)]
-    #[derive(Debug)]
     pub struct Approval {
         #[ink(topic)]
         owner: AccountId,
@@ -35,9 +27,6 @@ pub mod button {
         spender: AccountId,
         value: Balance,
     }
-
-    // === TYPES ===
-    pub type Event = <Button as ContractEventBase>::Type;
 
     // === STRUCTS ===
     #[ink(storage)]
@@ -47,88 +36,121 @@ pub mod button {
         psp22: psp22::Data,
         #[storage_field]
         metadata: metadata::Data,
-        cap: Balance,
+        #[storage_field]
+        cap: capped::Data,
     }
-    impl PSP22 for Button {}
-    impl PSP22Burnable for Button {}
-    impl PSP22Metadata for Button {}
-    impl PSP22Transfer for Button {
-        fn _before_token_transfer(
-            &mut self,
-            _from: Option<&AccountId>,
-            _to: Option<&AccountId>,
-            _amount: &Balance,
-        ) -> Result<(), PSP22Error> {
-            if _from.is_none() && (self.total_supply() + _amount) > self.cap() {
-                return Err(PSP22Error::Custom(String::from("Cap exceeded")));
-            }
-            Ok(())
-        }
-    }
-    // emit events
-    // https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md
-    impl Internal for Button {
-        fn _emit_transfer_event(
-            &self,
-            _from: Option<AccountId>,
-            _to: Option<AccountId>,
-            _amount: Balance,
-        ) {
-            Button::emit_event(
-                self.env(),
-                Event::Transfer(Transfer {
-                    from: _from,
-                    to: _to,
-                    value: _amount,
-                }),
-            );
-        }
 
-        fn _emit_approval_event(&self, _owner: AccountId, _spender: AccountId, _amount: Balance) {
-            Button::emit_event(
-                self.env(),
-                Event::Approval(Approval {
-                    owner: _owner,
-                    spender: _spender,
-                    value: _amount,
-                }),
-            );
-        }
+    #[overrider(psp22::Internal)]
+    fn _emit_transfer_event(
+        &self,
+        from: Option<AccountId>,
+        to: Option<AccountId>,
+        amount: Balance,
+    ) {
+        self.env().emit_event(Transfer {
+            from,
+            to,
+            value: amount,
+        });
     }
+
+    #[overrider(psp22::Internal)]
+    fn _emit_approval_event(&self, owner: AccountId, spender: AccountId, amount: Balance) {
+        self.env().emit_event(Approval {
+            owner,
+            spender,
+            value: amount,
+        });
+    }
+
     impl Button {
         #[ink(constructor)]
         pub fn new(
-            initial_supply: Balance,
+            cap: Balance,
             name: Option<String>,
             symbol: Option<String>,
             decimal: u8,
         ) -> Self {
             let mut instance = Self::default();
-            assert!(instance._init_cap(initial_supply).is_ok());
-            assert!(instance
-                ._mint_to(instance.env().caller(), initial_supply)
-                .is_ok());
-            instance.metadata.name = name;
-            instance.metadata.symbol = symbol;
-            instance.metadata.decimals = decimal;
+            assert!(capped::Internal::_init_cap(&mut instance, cap).is_ok());
+            assert!(psp22::Internal::_mint_to(&mut instance, Self::env().caller(), cap).is_ok());
+            instance.metadata.name.set(&name);
+            instance.metadata.symbol.set(&symbol);
+            instance.metadata.decimals.set(&decimal);
             instance
         }
 
         #[ink(message)]
-        pub fn cap(&self) -> Balance {
-            self.cap
-        }
+        pub fn burn(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
+            let caller = Self::env().caller();
+            if caller != account {
+                let allowance: Balance = psp22::Internal::_allowance(self, &account, &caller);
+                if allowance < amount {
+                    return Err(PSP22Error::InsufficientAllowance);
+                }
 
-        fn _init_cap(&mut self, cap: Balance) -> Result<(), PSP22Error> {
-            if cap == 0 {
-                return Err(PSP22Error::Custom(String::from("Cap must be above 0")));
+                psp22::Internal::_approve_from_to(self, account, caller, allowance - amount)?;
             }
-            self.cap = cap;
-            Ok(())
+            psp22::Internal::_burn_from(self, account, amount)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use ink::env::{
+            test::{default_accounts, set_caller, DefaultAccounts},
+            DefaultEnvironment,
+        };
+
+        // === HELPERS ===
+        fn init() -> (DefaultAccounts<DefaultEnvironment>, Button) {
+            let accounts = default_accounts();
+            set_caller::<DefaultEnvironment>(accounts.bob);
+            let az_button = Button::new(
+                28_000_000_000_000,
+                Some("Button".to_string()),
+                Some("BTN".to_string()),
+                6,
+            );
+            (accounts, az_button)
         }
 
-        pub fn emit_event<EE: EmitEvent<Self>>(emitter: EE, event: Event) {
-            emitter.emit_event(event);
+        // === TEST HANDLES ===
+        #[ink::test]
+        fn test_burn() {
+            let (accounts, mut az_button) = init();
+            // when burning from own account
+            // = when balance is sufficient
+            // = * it burns the amount
+            az_button.burn(accounts.bob, 1_000_000_000_000).unwrap();
+            let mut balance: Balance = PSP22::balance_of(&az_button, accounts.bob);
+            assert_eq!(balance, 27_000_000_000_000);
+            // = when balance is insufficient
+            let mut result = az_button.burn(accounts.bob, 28_000_000_000_000);
+            // = * it raises an error
+            assert_eq!(result, Err(PSP22Error::InsufficientBalance));
+            // when burning from someone else's account
+            // = when balance is sufficient
+            // == when allowance is insufficient
+            set_caller::<DefaultEnvironment>(accounts.alice);
+            // == * it raises an error
+            result = az_button.burn(accounts.bob, 27_000_000_000_000);
+            assert_eq!(result, Err(PSP22Error::InsufficientAllowance));
+            // == when allowance is sufficient
+            set_caller::<DefaultEnvironment>(accounts.bob);
+            PSP22::increase_allowance(&mut az_button, accounts.alice, 28_000_000_000_000).unwrap();
+            set_caller::<DefaultEnvironment>(accounts.alice);
+            az_button.burn(accounts.bob, 1_000_000_000_000).unwrap();
+            // == * it burns the amount
+            balance = PSP22::balance_of(&az_button, accounts.bob);
+            assert_eq!(balance, 26_000_000_000_000);
+            // == * it decreases the allowance
+            let allowance: Balance = PSP22::allowance(&az_button, accounts.bob, accounts.alice);
+            assert_eq!(allowance, 27_000_000_000_000);
+            // === when balance is insufficient
+            result = az_button.burn(accounts.bob, 27_000_000_000_000);
+            assert_eq!(result, Err(PSP22Error::InsufficientBalance));
         }
     }
 }
